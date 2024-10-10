70b3925ca61a9694b751ea18f226561cfbe415c7
BjoniMiraka <bjoni.miraka@fshnstudent.info>
2024-10-10 18:34:44 +0200

    f

--- src/app/Home/Home.tsx
+++ src/app/Home/Home.tsx
@@ -2,9 +2,10 @@ import { Car } from "phosphor-react";
 import { BgSvg } from "../../assets/icons/BgSvg";
 import { Button } from "../../libs/shadcn/Button";
 import image from "./../../assets/images/home.png";
+import mercedes from "./../../assets/images/mercedes.png";
 export const Home = () => {
   return (
-    <div className="min-h-[calc(100vh-72px)] flex items-center md:justify-between justify-center mt-[72px] overflow-hidden relative ">
+    <div className="min-h-[calc(100vh-72px)] flex items-center md:justify-between justify-center mt-[72px] overflow-hidden relative flex-col-reverse md:flex-row">
       <div className="max-w-[37rem] md:pl-36 md:mr-7 space-y-6 px-4">
         <h1 className="font-semibold  text-black-10 lg:text-5xl text-3xl ">
           Find, book and rent a car{" "}
@@ -18,6 +19,7 @@ export const Home = () => {
           Lets Ride <Car size="20" />
         </Button>
       </div>
+      <img src={mercedes} className="w-80 md:hidden " alt="mercedes" />
       <BgSvg className="absolute right-0 top-0 z-[1] lg:block hidden" />
       <div className="max-w-[740px] w-full z-[2] md:block hidden">
         <img className="self-end " src={image} alt="" />
