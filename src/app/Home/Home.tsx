7ba00223c30ad8320d1153f78a0a48e752d2e64f
BjoniMiraka <bjoni.miraka@fshnstudent.info>
2024-10-10 18:16:11 +0200

    res

--- src/app/Home/Home.tsx
+++ src/app/Home/Home.tsx
@@ -4,10 +4,10 @@ import { Button } from "../../libs/shadcn/Button";
 import image from "./../../assets/images/home.png";
 export const Home = () => {
   return (
-    <div className="min-h-[calc(100vh-72px)] flex items-center justify-between mt-[72px] overflow-hidden relative">
-      <div className="max-w-[37rem] pl-36 mr-7 space-y-6">
+    <div className="min-h-[calc(100vh-72px)] flex items-center md:justify-between justify-center mt-[72px] overflow-hidden relative">
+      <div className="max-w-[37rem] md:pl-36 md:mr-7 space-y-6">
         <h1 className="font-semibold  text-black-10 lg:text-5xl text-3xl ">
-          Find, book and rent <br /> a car{" "}
+          Find, book and rent a car{" "}
           <span className="text-blue-500">Easily</span>
         </h1>
         <p className="text-black-100">
@@ -19,7 +19,7 @@ export const Home = () => {
         </Button>
       </div>
       <BgSvg className="absolute right-0 top-0 z-[1] lg:block hidden" />
-      <div className="max-w-[740px] w-full z-[2]">
+      <div className="max-w-[740px] w-full z-[2] md:block hidden">
         <img className="self-end " src={image} alt="" />
       </div>
     </div>
